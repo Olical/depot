473980baeb0077cc304acaf5e1ffed45a8b5e705
Oliver Caldwell <olliec87@gmail.com>
2018-03-22 13:33:58 +0000

    Full resolution of every dep under an alias

--- src/depot/main.clj
+++ src/depot/main.clj
@@ -1,2 +1,11 @@
 (ns depot.main
-  (:require [clojure.tools.deps.alpha :as deps]))
+  (:require [clojure.tools.deps.alpha :as deps]
+            [clojure.tools.deps.alpha.reader :as reader]))
+
+(let [deps-map (-> (reader/clojure-env)
+                   (:config-files)
+                   (reader/read-deps))
+      args-map (deps/combine-aliases deps-map #{:test})]
+  {:deps deps-map
+   :args args-map
+   :resolved (deps/resolve-deps deps-map args-map)})
