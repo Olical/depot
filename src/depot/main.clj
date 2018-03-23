9b63af623d7fc0f9e1c4acf54bfa713216f8c996
Oliver Caldwell <olliec87@gmail.com>
2018-03-23 11:54:33 +0000

    Printing current and all remote versions

--- src/depot/main.clj
+++ src/depot/main.clj
@@ -2,22 +2,26 @@
   (:require [clojure.tools.deps.alpha :as deps]
             [clojure.tools.deps.alpha.reader :as reader]
             [clojure.tools.deps.alpha.util.maven :as maven])
-  (:import [org.eclipse.aether.resolution VersionRangeRequest]))
+  (:import [org.eclipse.aether.resolution VersionRangeRequest]
+           [org.eclipse.aether.version Version]))
 
 ;; TODO Make this configurable.
 (def aliases #{:test})
 
-(defn coord->versions [[lib coord] {:keys [mvn/repos mvn/local-repo]}]
+(defn coord->version-status [lib coord {:keys [mvn/repos mvn/local-repo]}]
   (let [local-repo (or local-repo maven/default-local-repo)
         remote-repos (mapv maven/remote-repo repos)
         system (maven/make-system)
         session (maven/make-session system local-repo)
-        artifact (maven/coord->artifact lib (assoc coord :mvn/version "[0,)"))
+        artifact (maven/coord->artifact lib coord)
+        artifact-range (maven/coord->artifact lib (assoc coord :mvn/version "[0,)"))
         versions-req (doto (new VersionRangeRequest)
-                       (.setArtifact artifact)
+                       (.setArtifact artifact-range)
                        (.setRepositories remote-repos))
-        versions-res (.resolveVersionRange system session versions-req)]
-    (.getVersions versions-res)))
+        versions (-> (.resolveVersionRange system session versions-req)
+                     (.getVersions))]
+    {:current (.getVersion artifact)
+     :newer (map str versions)}))
 
 (let [deps-map (-> (reader/clojure-env)
                    (:config-files)
@@ -26,4 +30,6 @@
       resolved-universe (deps/resolve-deps deps-map args-map)
       direct-deps (keys (merge (:deps deps-map) (:extra-deps args-map)))
       resolved-local (select-keys resolved-universe direct-deps)]
-  (last (coord->versions (first resolved-local) deps-map)))
+  (doseq [[lib coord] resolved-local]
+    (let [{:keys [current newer]} (coord->version-status lib coord deps-map)]
+      (prn [lib current newer]))))
