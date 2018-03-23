5456c5b994c5d793ad1481f6aa554026dea084e8
Oliver Caldwell <olliec87@gmail.com>
2018-03-23 12:02:18 +0000

    Add version-clj so I can parse and filter versions

--- src/depot/main.clj
+++ src/depot/main.clj
@@ -1,9 +1,9 @@
 (ns depot.main
   (:require [clojure.tools.deps.alpha :as deps]
             [clojure.tools.deps.alpha.reader :as reader]
-            [clojure.tools.deps.alpha.util.maven :as maven])
-  (:import [org.eclipse.aether.resolution VersionRangeRequest]
-           [org.eclipse.aether.version Version]))
+            [clojure.tools.deps.alpha.util.maven :as maven]
+            [version-clj.core :as version])
+  (:import [org.eclipse.aether.resolution VersionRangeRequest]))
 
 ;; TODO Make this configurable.
 (def aliases #{:test})
@@ -13,15 +13,16 @@
         remote-repos (mapv maven/remote-repo repos)
         system (maven/make-system)
         session (maven/make-session system local-repo)
-        artifact (maven/coord->artifact lib coord)
-        artifact-range (maven/coord->artifact lib (assoc coord :mvn/version "[0,)"))
+        current (:mvn/version coord)
+        artifact (maven/coord->artifact lib (assoc coord :mvn/version "[0,)"))
         versions-req (doto (new VersionRangeRequest)
-                       (.setArtifact artifact-range)
+                       (.setArtifact artifact)
                        (.setRepositories remote-repos))
-        versions (-> (.resolveVersionRange system session versions-req)
-                     (.getVersions))]
-    {:current (.getVersion artifact)
-     :newer (map str versions)}))
+        versions (->> (.resolveVersionRange system session versions-req)
+                      (.getVersions)
+                      (map str))]
+    {:current current
+     :newer versions}))
 
 (let [deps-map (-> (reader/clojure-env)
                    (:config-files)
