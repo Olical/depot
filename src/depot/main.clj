28fac15274da5bd7d44b6415e0778da1d0e60ff2
Oliver Caldwell <olliec87@gmail.com>
2018-03-23 11:24:35 +0000

    Local required deps can have their versions listed!

--- src/depot/main.clj
+++ src/depot/main.clj
@@ -1,14 +1,29 @@
 (ns depot.main
   (:require [clojure.tools.deps.alpha :as deps]
-            [clojure.tools.deps.alpha.reader :as reader]))
+            [clojure.tools.deps.alpha.reader :as reader]
+            [clojure.tools.deps.alpha.util.maven :as maven])
+  (:import [org.eclipse.aether.resolution VersionRangeRequest]))
 
 ;; TODO Make this configurable.
 (def aliases #{:test})
+
+(defn coord->versions [[lib coord] {:keys [mvn/repos mvn/local-repo]}]
+  (let [local-repo (or local-repo maven/default-local-repo)
+        remote-repos (mapv maven/remote-repo repos)
+        system (maven/make-system)
+        session (maven/make-session system local-repo)
+        artifact (maven/coord->artifact lib (assoc coord :mvn/version "[0,)"))
+        versions-req (doto (new VersionRangeRequest)
+                       (.setArtifact artifact)
+                       (.setRepositories remote-repos))
+        versions-res (.resolveVersionRange system session versions-req)]
+    (.getVersions versions-res)))
 
 (let [deps-map (-> (reader/clojure-env)
                    (:config-files)
                    (reader/read-deps))
       args-map (deps/combine-aliases deps-map aliases)
-      resolved (deps/resolve-deps deps-map args-map)
-      direct-deps (keys (merge (:deps deps-map) (:extra-deps args-map)))]
-  (select-keys resolved direct-deps))
+      resolved-universe (deps/resolve-deps deps-map args-map)
+      direct-deps (keys (merge (:deps deps-map) (:extra-deps args-map)))
+      resolved-local (select-keys resolved-universe direct-deps)]
+  (last (coord->versions (first resolved-local) deps-map)))
