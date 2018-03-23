1441a5a96d6391f74dc5a2d60564db3d954f3f8b
Oliver Caldwell <olliec87@gmail.com>
2018-03-23 10:37:28 +0000

    Now resolving all local deps with aliases

--- src/depot/main.clj
+++ src/depot/main.clj
@@ -2,10 +2,13 @@
   (:require [clojure.tools.deps.alpha :as deps]
             [clojure.tools.deps.alpha.reader :as reader]))
 
+;; TODO Make this configurable.
+(def aliases #{:test})
+
 (let [deps-map (-> (reader/clojure-env)
                    (:config-files)
                    (reader/read-deps))
-      args-map (deps/combine-aliases deps-map #{:test})]
-  {:deps deps-map
-   :args args-map
-   :resolved (deps/resolve-deps deps-map args-map)})
+      args-map (deps/combine-aliases deps-map aliases)
+      resolved (deps/resolve-deps deps-map args-map)
+      direct-deps (keys (merge (:deps deps-map) (:extra-deps args-map)))]
+  (select-keys resolved direct-deps))
