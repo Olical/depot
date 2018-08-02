d18197d0fc41cc9b0d2b77c12dd1c6e5ecaead7c
Kenny Williams <kenny@computesoftware.com>
2018-08-01 17:13:58 -0700

    Add support for git dependencies.

--- src/depot/outdated/main.clj
+++ src/depot/outdated/main.clj
@@ -2,10 +2,12 @@
   (:require [clojure.pprint :as pprint]
             [clojure.set :as set]
             [clojure.string :as str]
+            [clojure.java.shell :as sh]
             [clojure.tools.cli :as cli]
             [clojure.tools.deps.alpha :as deps]
             [clojure.tools.deps.alpha.reader :as reader]
             [clojure.tools.deps.alpha.util.maven :as maven]
+            [clojure.tools.deps.alpha.extensions :as ext]
             [version-clj.core :as version])
   (:import [org.eclipse.aether.resolution VersionRangeRequest]))
 
@@ -44,24 +46,66 @@
     (-> (sort version/version-compare versions)
         (last))))
 
+(defmulti -current-latest-map (fn [_ coord _] (ext/coord-type coord)))
+
+(defmethod -current-latest-map :default
+  [_ _ _]
+  nil)
+
+(defmethod -current-latest-map :mvn
+  [lib coord {:keys [deps-map consider-types]}]
+  (let [{:keys [types selected]} (coord->version-status lib coord deps-map)
+        latest                   (find-latest types consider-types)]
+    (when (and (not (str/blank? selected))
+               (not (str/blank? latest))
+               (= (version/version-compare latest selected) 1))
+      {"Current" selected
+       "Latest"  latest})))
+
+(defn- parse-git-ls-remote
+  "Returns a map of ref name to the latest sha for that ref name."
+  [s]
+  (let [lines (-> s
+                  (str/trim-newline)
+                  (str/split #"\n"))]
+    (into {}
+          (comp
+            (map (fn [x]
+                   (-> x
+                       (str/triml)
+                       (str/split #"\t")
+                       (reverse)
+                       (vec))))
+            (filter #(= 2 (count %))))
+          lines)))
+
+(defmethod -current-latest-map :git
+  [lib coord _]
+  (let [{:keys [exit out err]} (sh/sh "git" "ls-remote" (:git/url coord))
+        latest-remote-sha (get (parse-git-ls-remote out) "HEAD")]
+    (when (and (= exit 0)
+               (neg? (ext/compare-versions
+                       lib coord (assoc coord :sha latest-remote-sha) {})))
+      {"Current" (:sha coord)
+       "Latest"  latest-remote-sha})))
+
+(defn current-latest-map
+  "Returns a map containing `'Current'` and `'Latest'` if the dependency has a
+  newer version otherwise returns `nil`."
+  [lib coord data]
+  (-current-latest-map lib coord data))
+
 (defn gather-outdated [consider-types aliases]
   (let [deps-map (-> (reader/clojure-env)
                      (:config-files)
                      (reader/read-deps))
         args-map (deps/combine-aliases deps-map aliases)
-        resolved-universe (deps/resolve-deps deps-map args-map)
-        direct-deps (->> (merge (:deps deps-map) (:extra-deps args-map))
-                         (filter (comp :mvn/version val))
-                         (map first))]
-    (->> (for [[lib coord] (select-keys resolved-universe direct-deps)]
-           (let [{:keys [types selected]} (coord->version-status lib coord deps-map)
-                 latest                   (find-latest types consider-types)]
-             (when (and (not (str/blank? selected))
-                        (not (str/blank? latest))
-                        (= (version/version-compare latest selected) 1))
-               {"Dependency" lib
-                "Current"    selected
-                "Latest"     latest})))
+        all-deps (merge (:deps deps-map) (:extra-deps args-map))]
+    (->> (for [[lib coord] all-deps
+               :let [outdated (current-latest-map lib coord {:consider-types consider-types
+                                                             :deps-map       deps-map})]]
+           (when outdated
+             (assoc outdated "Dependency" lib)))
          (keep identity)
          (sort-by #(get % "Dependency")))))
 
