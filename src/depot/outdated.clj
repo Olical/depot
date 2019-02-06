996601d07dc21ef44964663db8519e217084865b
Laverne Schrock <lschrock@roomkey.com>
2019-02-05 20:14:58 -0500

    Use pmap to check lib versions in parallel

--- src/depot/outdated.clj
+++ src/depot/outdated.clj
@@ -100,14 +100,14 @@
         overrides (:override-deps args-map)
         all-deps (merge (:deps deps-map) (:extra-deps args-map)
                         (when include-overrides overrides))]
-    (->> (for [[lib coord] all-deps
-               :let [outdated (current-latest-map lib
-                                                  (if include-overrides
-                                                    coord
-                                                    (get overrides lib coord))
-                                                  {:consider-types consider-types
-                                                   :deps-map       deps-map})]]
-           (when outdated
-             (assoc outdated "Dependency" lib)))
+    (->> (pmap (fn [[lib coord]]
+                 (let [outdated (current-latest-map lib
+                                                    (if include-overrides
+                                                      coord
+                                                      (get overrides lib coord))
+                                                    {:consider-types consider-types
+                                                     :deps-map       deps-map})]
+                   (when outdated
+                     (assoc outdated "Dependency" lib)))) all-deps)
          (keep identity)
          (sort-by #(get % "Dependency")))))
