750059131b0a14e3e9a3c18ea4f814d734e01ae8
Laverne Schrock <lschrock@roomkey.com>
2019-09-15 19:05:35 -0400

    Remove unused require in update test

--- test/depot/outdated/update_test.clj
+++ test/depot/outdated/update_test.clj
@@ -3,7 +3,6 @@
             [depot.outdated.update :as u]
             [rewrite-clj.node :as node]
             [rewrite-clj.zip :as rzip]
-            [rewrite-clj.node :as node]
             [clojure.tools.deps.alpha.util.maven :as maven]))
 
 (def CONSIDER_TYPES_RELEASES #{:release})
