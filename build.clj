db6ddefe66b9ad688a5fd54c0d095a9970d5232f
Oliver Caldwell <ollie@oli.me.uk>
2025-07-11 13:42:29 +0100

    Replace makefile and scripts with mise from github.com/Olical/clojure-template

---
+++ build.clj
@@ -0,0 +1,54 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]
+            [deps-deploy.deps-deploy :as dd]))
+
+(def lib 'uk.me.oli/depot)
+
+;; TODO Use the GitHub rev / tag
+;; https://github.com/jlesquembre/clojars-publish-action/blob/89a4eb7bdbe1270621e6643250afce152701699e/src/entrypoint.clj#L41-L47
+(def version "2.4.0") ; (b/git-count-revs nil)
+
+(def class-dir "target/classes")
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
+
+(def pom-template
+  [[:description "Find newer versions of your dependencies in your deps.edn file."]
+   [:url "https://github.com/Olical/depot"]
+   [:licenses
+    [:license
+     [:name "Unlicense"]
+     [:url "https://unlicense.org/"]]]
+   [:developers
+    [:developer
+     [:name "Oliver Caldwell"]]]
+   [:scm
+    [:url "https://github.com/Olical/depot"]
+    [:connection "scm:git:https://github.com/Olical/depot.git"]
+    [:developerConnection "scm:git:ssh://git@github.com:Olical/depot.git"]
+    [:tag (str "v" version)]]])
+
+(def basis (delay (b/create-basis {:project "deps.edn"})))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn jar [_]
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version version
+                :basis @basis
+                :pom-data pom-template
+                :src-dirs ["src"]})
+
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file}))
+
+(defn deploy "Deploy the JAR to Clojars." [opts]
+  (dd/deploy {:installer :remote :artifact (b/resolve-path jar-file)
+              :pom-file (b/pom-path
+                         {:lib lib
+                          :class-dir class-dir})})
+  opts)
--- dev/depot/dev/cider.clj
+++
@@ -1,5 +0,0 @@
-(ns depot.dev.cider
-  (:require [cider-nrepl.main :as nrepl]))
-
-(defn -main []
-  (nrepl/init ["cider.nrepl/cider-middleware"]))
